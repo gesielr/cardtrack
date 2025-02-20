fef0705c27f21cf81c3e0c1770ec471786f19fc2
gesielr <carlosgesiell@gmail.com>
2025-02-19 23:41:57 -0300

    Ajustes para GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // 🚀 Importa o React Router
+import { BrowserRouter } from "react-router-dom"; // 
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/responsive.css"; // Importando os estilos responsivos
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
